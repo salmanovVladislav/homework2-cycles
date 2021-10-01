498dbfd2bc61d623a540972edeba932dbdf14ad2
Владислав <salmanov_vlad@mail.ru>
2021-10-01 22:02:44 +0300

    Homework 5 is done

--- src/pro/sky/java/course1/lesson5/Main.java
+++ src/pro/sky/java/course1/lesson5/Main.java
@@ -2,28 +2,46 @@ package pro.sky.java.course1.lesson5;
 
 public class Main {
         public static void main(String[] args) {
-                basictasks();
+                task1();
+                task2();
+                task3();
+                task4();
         }
 
-        private static void basictasks() {
+        private static void task1() {
                 String firstName = "Ivan";
                 String middleName = "Ivanovich";
                 String lastName = "Ivanov";
                 String fullName = lastName + " " + firstName + " " + middleName;
                 System.out.println("ФИО сотрудника - " + fullName);
+        }
 
+        private static void task2() {
+                String firstName = "Ivan";
+                String middleName = "Ivanovich";
+                String lastName = "Ivanov";
+                String fullName = lastName + " " + firstName + " " + middleName;
                 String fullNameForReport = fullName.toUpperCase();
                 System.out.println("Данные ФИО сотрудника для заполнения отчета – " + fullNameForReport);
+        }
 
+        public static void task3() {
+                String firstName = "Ivan";
+                String middleName = "Ivanovich";
+                String lastName = "Ivanov";
+                String fullName = lastName + " " + firstName + " " + middleName;
                 String fullNameForPasses = fullName.replace(" ", ";");
                 System.out.println("Данные ФИО сотрудника для административного отдела - " + fullNameForPasses);
+        }
 
-                String[] fullName2 = fullName.split(" ");
-                fullName2[0] = "Иванов";
-                fullName2[1] = "Семён";
-                fullName2[2] = "Семёнович";
-                String fullName3 = fullName2[0] + " " + fullName2[1] + " " + fullName2[2];
-                String fullName4 = fullName3.replace('ё', 'е');
-                System.out.println(fullName4);
+        public static void task4() {
+                String firstName = "Иванов";
+                String middleName = "Семён";
+                String lastName = "Семёнович";
+                String fullName = firstName + " " + middleName + " " + lastName;
+                String fullName2 = fullName.replace('ё', 'е');
+                System.out.println("Данные ФИО сотрудника - " + fullName2);
+                //судя по всему, я не понял условие задачи..подсказки, которые даны в задаче (метод
+                //split и метод contains) не придумал как применить.
         }
 }
